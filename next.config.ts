016a9a4f9ca9343314ae78473c41cc1fcd518e3d
A1-Staffing <a1staffingprollc@gmail.com>
2025-02-18 18:05:02 +0530

    redirect done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+     
+      {
+        source: "/employers.html",
+        destination: "/employer",
+        permanent: true,
+      },
+      {
+        source: "/contact.html",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/accounting-finance-employees-for-business-in-New-Jersey.html",
+        destination: "/accounting-finance-employees-for-business-in-New-Jersey",
+        permanent: true,
+      },
+      {
+        source: "/locations.html",
+        destination: "/location",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
